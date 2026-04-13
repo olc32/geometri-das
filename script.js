fed51f9f101e4186369897628d9e314cad3744b2
olc32 <7449894@alu.murciaeduca.es>
2026-04-13 18:29:38 +0200

    act2

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var juego = document.getElementById("juego");
+var body = document.body;
 var cube = document.getElementById("Cube");
-juego.addEventListener("click", function() {
-    
+cube.addEventListener("click", function() {
+    cube.style.bottom = "100px";
 });
+//Gravedad
+while (true) {
+    if (Intcube.style.bottom <= "0px") {
+        cube.style.bottom = "0px";
+    }
