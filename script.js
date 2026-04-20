68d35850d6306dc131352fb8cd095d40b74b1133
olc32 <tegif89175@bmoar.com>
2026-04-20 18:07:05 +0200

    Actu3normal

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 var juego = document.getElementById("juego");
 var body = document.body;
-var cube = document.getElementById("Cube");
-var gravity = 1;
+var cube = document.getElementById("cube");
+var gravity = -1;
 var velocity = 0;
+var y = 0;
+
+cube.style.position = "absolute";
+cube.style.bottom = "0px";
 window.addEventListener("click", function() {
-    velocity = -15;
+    velocity = 15;
 });
 
 function gameLoop() {
     velocity += gravity;
-    cube.style.bottom = parseInt(cube.style.bottom) - velocity + "px";
-    if (parseInt(cube.style.bottom) <= 0) {
-        cube.style.bottom = "0px";
+    y += velocity;
+    cube.style.bottom = y + "px";
+    if (parseInt(y) <= 0) {
+        y = 0;
         velocity = 0;
     }
+    console.log("Velocity: " + velocity + " | Bottom: " + cube.style.bottom + " | Y: " + y);
     requestAnimationFrame(gameLoop);
 }
 
